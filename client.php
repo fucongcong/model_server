c3888cd712f3d98db4be054c5b285e52abcc3f11
fucongcong <cc@xitongxue.com>
2015-09-22 14:07:17 +0800

    add client

--- client.php
+++ client.php
@@ -7,3 +7,5 @@ $loader->setUseIncludePath(true);
 require __DIR__.'/vendor/hprose/hprose/src/Hprose.php';
 
 $client = new HproseSwooleClient('tcp://127.0.0.1:9394');
+
+var_dump($client->Special_Special_getSpecial(48));
